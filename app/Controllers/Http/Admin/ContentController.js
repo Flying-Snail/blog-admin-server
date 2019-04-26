79693858c7f32e723431c396b48571695392663c
jiasonglin <jiasonglin001@ke.com>
2019-04-26 13:09:53 +0800

    内容管理

--- app/Controllers/Http/Admin/ContentController.js
+++ app/Controllers/Http/Admin/ContentController.js
@@ -1,19 +1,16 @@
 'use strict'
 
-const Post = use('App/Models/Post')
-const Label = use('App/Models/Label')
-
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 /**
- * Resourceful controller for interacting with posts
+ * Resourceful controller for interacting with contents
  */
-class PostController {
+class ContentController {
   /**
-   * Show a list of all posts.
-   * GET posts
+   * Show a list of all contents.
+   * GET contents
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -21,14 +18,11 @@ class PostController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
-    const page = request.get().page || 1
-    const posts = await Post.where({}).sort('-is_top').paginate(page)
-    return posts
   }
 
   /**
-   * Render a form to be used for creating a new post.
-   * GET posts/create
+   * Render a form to be used for creating a new content.
+   * GET contents/create
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -39,33 +33,19 @@ class PostController {
   }
 
   /**
-   * Create/save a new post.
-   * POST posts
+   * Create/save a new content.
+   * POST contents
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
-    const data = Object.assign({}, request.post(), {
-      is_deleted: false,
-      like_num: 0,
-      comment_num: 0
-    })
-    const modelPost = new Post()
-    modelPost.fill(data)
-    await modelPost.save()
-    const modelLabel = new Label()
-    const postId = modelPost._id
-    modelLabel.fill({
-      
-    })
-    return modelPost
   }
 
   /**
-   * Display a single post.
-   * GET posts/:id
+   * Display a single content.
+   * GET contents/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -73,12 +53,11 @@ class PostController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-    return await Post.find(params.id)
   }
 
   /**
-   * Render a form to update an existing post.
-   * GET posts/:id/edit
+   * Render a form to update an existing content.
+   * GET contents/:id/edit
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -86,40 +65,29 @@ class PostController {
    * @param {View} ctx.view
    */
   async edit ({ params, request, response, view }) {
-    return await Post.find(params.id)
   }
 
   /**
-   * Update post details.
-   * PUT or PATCH posts/:id
+   * Update content details.
+   * PUT or PATCH contents/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
   async update ({ params, request, response }) {
-    const data = request.post()
-    const model = await Post.find(params.id)
-    model.merge(data)
-    await model.save()
-    return model
   }
 
   /**
-   * Delete a post with id.
-   * DELETE posts/:id
+   * Delete a content with id.
+   * DELETE contents/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
   async destroy ({ params, request, response }) {
-    const model = await Post.find(params.id)
-    await model.delete()
-    return {
-      success: true
-    }
   }
 }
 
-module.exports = PostController
+module.exports = ContentController
--- app/Controllers/Http/Admin/LabelController.js
+++ app/Controllers/Http/Admin/LabelController.js
@@ -1,19 +1,16 @@
 'use strict'
 
-const Post = use('App/Models/Post')
-const Label = use('App/Models/Label')
-
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
 /**
- * Resourceful controller for interacting with posts
+ * Resourceful controller for interacting with labels
  */
-class PostController {
+class LabelController {
   /**
-   * Show a list of all posts.
-   * GET posts
+   * Show a list of all labels.
+   * GET labels
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -21,14 +18,11 @@ class PostController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
-    const page = request.get().page || 1
-    const posts = await Post.where({}).sort('-is_top').paginate(page)
-    return posts
   }
 
   /**
-   * Render a form to be used for creating a new post.
-   * GET posts/create
+   * Render a form to be used for creating a new label.
+   * GET labels/create
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -39,33 +33,19 @@ class PostController {
   }
 
   /**
-   * Create/save a new post.
-   * POST posts
+   * Create/save a new label.
+   * POST labels
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
-    const data = Object.assign({}, request.post(), {
-      is_deleted: false,
-      like_num: 0,
-      comment_num: 0
-    })
-    const modelPost = new Post()
-    modelPost.fill(data)
-    await modelPost.save()
-    const modelLabel = new Label()
-    const postId = modelPost._id
-    modelLabel.fill({
-      
-    })
-    return modelPost
   }
 
   /**
-   * Display a single post.
-   * GET posts/:id
+   * Display a single label.
+   * GET labels/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -73,12 +53,11 @@ class PostController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-    return await Post.find(params.id)
   }
 
   /**
-   * Render a form to update an existing post.
-   * GET posts/:id/edit
+   * Render a form to update an existing label.
+   * GET labels/:id/edit
    *
    * @param {object} ctx
    * @param {Request} ctx.request
@@ -86,40 +65,29 @@ class PostController {
    * @param {View} ctx.view
    */
   async edit ({ params, request, response, view }) {
-    return await Post.find(params.id)
   }
 
   /**
-   * Update post details.
-   * PUT or PATCH posts/:id
+   * Update label details.
+   * PUT or PATCH labels/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
   async update ({ params, request, response }) {
-    const data = request.post()
-    const model = await Post.find(params.id)
-    model.merge(data)
-    await model.save()
-    return model
   }
 
   /**
-   * Delete a post with id.
-   * DELETE posts/:id
+   * Delete a label with id.
+   * DELETE labels/:id
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
   async destroy ({ params, request, response }) {
-    const model = await Post.find(params.id)
-    await model.delete()
-    return {
-      success: true
-    }
   }
 }
 
-module.exports = PostController
+module.exports = LabelController
